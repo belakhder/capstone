69620b110ff51364e5b6c20375796e0489a6e07d
princeofpersia1990 <fbelakhdhar@live.fr>
2020-07-19 16:10:04 +0100

    change

--- tests/test_app.py
+++ tests/test_app.py
@@ -45,6 +45,7 @@ class AccountTestCase(unittest.TestCase):
             headers = self.administrator_auth_header)
         data = json.loads(response.data)
         print(self.administrator_auth_header)
+        print(self.administrator_auth_header)
         
         
 
